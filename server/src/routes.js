4bb785408a9731246657236cb49e34d84ac47540
Guilherme Pim <guilherme.pim@stone.com.br>
2023-08-06 22:17:33 -0300

    feat: adding opengraph

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const fs = require('fs')
 const Utils = require('./lib/utils')
+const got = require('got')
 
 const routes = express.Router();
+
+// We will cache the index using HTTP when we first try to serve the dynamic index
+let cachedIndexPage = undefined 
 
 //
 // Métodos helpers
@@ -12,9 +16,20 @@ const routes = express.Router();
 // (isso são informacoes para compartilhamento de link em plataformas como discord, etc)
 
 // Mais detalhes aqui: https://ogp.me/
-const serveIndexWithOpenGraph = function (req, res) {
+const serveIndexWithOpenGraph = async function (req, res) {
 
-  // let contents = fs.readFileSync('../client/build/index.html').toString()
+  if (!cachedIndexPage) {
+    const {body} = await got(process.env.CLIENT_HTML_INDEX_URL)
+    cachedIndexPage = body
+    console.log("Just loaded index page: %s", body)
+  }
+
+  if (!cachedIndexPage) {
+    res.send('Eita, deu ruim :o')
+    return
+  }
+
+  console.log("FORWARDED FOR", req.headers['x-forwarded-for'])
 
   // Mais detalhes aqui: https://ogp.me/
   // Com esse esquema de "opengraph", plataformas como discord e facebook conseguem
@@ -34,8 +49,7 @@ const serveIndexWithOpenGraph = function (req, res) {
 <meta property="og:description" content="${opengraphDescription}" />
   `;
 
-  // contents = contents.replace("<opengraph/>", opengraphData)
-  res.send(opengraphData)
+  res.send(cachedIndexPage.replace("<opengraph/>", opengraphData))
 }
 
 
@@ -50,12 +64,19 @@ routes.get("/healthcheck", (req, res) => {
 
 // Vamos renderizar o / com script e não estático, para poder
 // mandar parametros de opengraph :)
-// routes.get("/", serveIndexWithOpenGraph)
-routes.get("/GameRoom/:roomName", serveIndexWithOpenGraph)
 
-// E servir o arquivo index.html em caso de 404,
-// que deve saber lidar com isso no frontend
-// routes.use(serveIndexWithOpenGraph)
+if (process.env.CLIENT_HTML_INDEX_URL) {
+  console.log("Serving OpenGraph contents in / and /GameRoom/:roomName and 404 pages from [%s]", process.env.CLIENT_HTML_INDEX_URL)
+  routes.get("/", serveIndexWithOpenGraph)
+  routes.get("/GameRoom/:roomName", serveIndexWithOpenGraph)
+  // E servir o arquivo index.html em caso de 404,
+  // que deve saber lidar com isso no frontend
+  routes.use(serveIndexWithOpenGraph)
+} else {
+  console.log("NOT serving OpenGraph contents in / and /GameRoom/:roomName")
+  
+}
+
 
 module.exports = routes;
 
